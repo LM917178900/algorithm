dea86265cb5fe71a8efbe98d34884b8183c4a09c
leimin <917178900@qq.com>
2020-03-31 18:53:02 +0800

    bubble sort

--- src/main/java/org/lei/algorithm/contoller/TestController.java
+++ src/main/java/org/lei/algorithm/contoller/TestController.java
@@ -1,5 +1,8 @@
 package org.lei.algorithm.contoller;
 
+import java.util.Arrays;
+import java.util.Random;
+
 /**
  * @Author: leimin
  * @Description: controller
@@ -7,4 +10,55 @@ package org.lei.algorithm.contoller;
  * @Version: 1.0
  */
 public class TestController {
+
+
+    /**
+     * source from next url
+     * https://www.cnblogs.com/nankeyimengningchenlun/p/9151701.html
+     * <p>
+     * target
+     * 1.sample of algorithm
+     * 2.test the speed of method in sort
+     */
+
+    public static void main(String[] args) {
+
+        int totalNumber = 100;
+        int[] arr = new int[totalNumber];
+        for (int i = 0; i < totalNumber; i++) {
+            arr[i] = new Random().nextInt(totalNumber);
+
+        }
+
+        long bubbleStart = System.currentTimeMillis();
+        BubbleSort(arr);
+        System.out.println("BubbleSort cost millisecond===>" + (System.currentTimeMillis() - bubbleStart));
+    }
+
+    /**
+     * bubble sort
+     * Time complexity :n2
+     * Spatial complexity :1
+     *
+     * @param arr array
+     */
+    private static void BubbleSort(int[] arr) {
+
+        int temp;
+        int len = arr.length;
+
+        for (int i = 0; i < len; i++) {
+
+            for (int j = 0; j < len - i - 1; j++) {
+
+                if (arr[j] > arr[j + 1]) {
+                    temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                }
+            }
+        }
+        System.out.println(Arrays.toString(arr));
+    }
+
 }
